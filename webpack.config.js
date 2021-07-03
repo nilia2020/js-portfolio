eafdfd3a534968bea5af60f0e6c891189ecc8eb7
nilia2020 <jorge.niglia@gmail.com>
2021-07-03 19:46:41 -0300

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  module: {
-    rules: [
-      {
-        // Test declara que extensión de archivos aplicara el loader. Empiezan con m y tienen extensión js. Se pone con una epresión regular
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
-      inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-      template: './public/index.html', // LA RUTA AL TEMPLATE HTML
-      filename: './dist/index.html' // NOMBRE FINAL DEL ARCHIVO
-    })
-  ]
-}
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    resolve: {
+        extensions: [".js"],
+    },
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader. Empiezan con m y tienen extensión js. Se pone con una epresión regular
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // plugin de html
+            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+            template: "./public/index.html", // LA RUTA AL TEMPLATE HTML
+            filename: "./index.html", // NOMBRE FINAL DEL ARCHIVO
+        }),
+        new MiniCssExtractPlugin(), //plugin de css
+    ],
+};

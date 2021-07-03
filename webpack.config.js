e8dbc2995c89e912ed14162bf06170c252be719d
nilia2020 <jorge.niglia@gmail.com>
2021-07-03 15:48:42 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader. Empiezan con m y tienen extensión js. Se pone con una epresión regular
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+      inject: 'body', // INYECTA EL BUNDLE AL TEMPLATE HTML
+      template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+      filename: './dist/index.html' // NOMBRE FINAL DEL ARCHIVO
+    })
+  ]
+}
